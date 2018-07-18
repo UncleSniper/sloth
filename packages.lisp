cfbdad4583b5977bffb46bb8b9ad3b297ecb7310
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-07-18 14:55:44 +0000

    started artifact.lisp

--- packages.lisp
+++ packages.lisp
@@ -13,3 +13,22 @@
    :walk-directory
    :directory-p
    :file-p))
+
+; artifact.lisp
+(defpackage :org.unclesniper.sloth.artifact
+  (:use
+   :common-lisp
+   :com.gigamonkeys.pathnames)
+  (:export
+   :*assume-all-out-of-date*
+   :artifact
+   :artifact-pathname
+   :artifact-type
+   :artifact-properties
+   :artifact-property
+   :set-artifact-property
+   :artifact-prerequisites
+   :add-artifact-prerequisites
+   :remove-artifact-prerequisites
+   :artifact-known-up-to-date-p
+   :artifact-out-of-date-p))
